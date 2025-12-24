cb6314b964ba4e3e4bc2850548b1df03a3239929
mshupliakou <mikhail.shupliakou@gmail.com>
2025-12-24 22:59:48 +0100

    register method

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+from . import db
+from .models import Module, User
 from .models import Module
 
 main = Blueprint('main', __name__)
@@ -16,3 +18,22 @@ def modules():
 @main.route('/login')
 def login():
     return render_template('login.html')
+
+@main.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user = User.query.filter_by(email=email).first()
+        if user:
+            return "User already exists."
+        new_user = User(username=username, email=email, role='user')
+        new_user.set_password(password)
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('main.login'))
+
+    return render_template('register.html')
