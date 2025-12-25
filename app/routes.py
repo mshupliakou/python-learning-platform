4397378decc91ba8b5d4047f59ed433241ca352a
mshupliakou <mikhail.shupliakou@gmail.com>
2025-12-25 12:27:23 +0100

    login function is implemented

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from . import db
+from flask_login import login_user, logout_user, login_required, current_user
 from .models import Module, User
 from .models import Module
 
@@ -12,12 +13,37 @@ def base():
 @main.route('/modules')
 def modules():
     all_courses = Module.query.all()
-    return render_template('modules.html')
+    return render_template('modules.html', courses=all_courses)
 
 
-@main.route('/login')
+@main.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.modules'))
+
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.check_password(password):
+            login_user(user)
+            return redirect(url_for('main.modules'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+            return "Wrong username or password"
+
     return render_template('login.html')
+
+
+@main.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
+
+
 
 @main.route('/register', methods=['GET', 'POST'])
 def register():
